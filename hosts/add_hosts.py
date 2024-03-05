64f631d8c61be18d71d96dcb563be9f2c760289e
Mociofleaca Alin <mociofleacaalin@Mociofleacas-MacBook-Pro.local>
2024-03-05 08:17:56 +0200

    enhance script

--- hosts/add_hosts.py
+++ hosts/add_hosts.py
@@ -17,8 +17,11 @@ remote_hosts_config = zapi.host.get({
 })
 
 # Convert lists of dictionaries to sets of tuples
-local_set = set((host) for host in local_hosts_config)
+local_set = set((host["hostid"]) for host in local_hosts_config)
 remote_set = set((host) for host in remote_hosts_config)
+
+local_only = [d for d in local_set if not any(d == d2 for d2 in remote_hosts_config)]
+remote_only = [d for d in remote_hosts_config if not any(d == d1 for d1 in local_set)]
 
 # Find elements in local, but not in remote, these should be added to keep in sync local config in git with remote
 local_only = local_set - remote_set

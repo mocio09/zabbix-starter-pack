4b286ec2c24343674ca6fe35eea8a569ea169b9c
Mociofleaca Alin <mociofleacaalin@Mociofleacas-MacBook-Pro.local>
2024-03-05 08:11:46 +0200

    enhance script

--- hosts/add_hosts.py
+++ hosts/add_hosts.py
@@ -2,13 +2,34 @@ from pyzabbix import ZabbixAPI
 import json
 import os
 
+# Get configuration for hosts
 with open('hosts_inventory.json', 'r') as f:
-    hosts_config = json.load(f)
+    local_hosts_config = json.load(f)
 
 zapi = ZabbixAPI("http://18.133.139.129/api_jsonrpc.php")
 zapi.login(os.getenv("ZABBIX_LOGIN_USER"), os.getenv("ZABBIX_LOGIN_PASSWORD"))
 
+# Get all Hosts from remote server
+remote_hosts_config = zapi.host.get({
+    "output": ["hostid", "host"],
+    "selectInterfaces": ["interfaceid", "ip", "dns", "type", "main", "useip", "port"],
+    "selectGroups": ["groupid", "name"]
+})
 
-for host in hosts_config:
+# Convert lists of dictionaries to sets of tuples
+local_set = set((host["host"], host["ip"]) for host in local_hosts_config)
+remote_set = set((host["host"], host["ip"]) for host in remote_hosts_config)
+
+# Find elements in local, but not in remote, these should be added to keep in sync local config in git with remote
+local_only = local_set - remote_set
+
+# Find elements in remote but not in local, , these should be deleted to keep in sync local config in git with remote
+remote_only = remote_set - local_set
+
+for host in local_only:
     result = zapi.host.create(host)
-    print(result)
+    print(f"Added host {host}")
+
+for host in remote_only:
+    result = zapi.host.delete(host["hostid"])
+    print(f"Added host {host}")

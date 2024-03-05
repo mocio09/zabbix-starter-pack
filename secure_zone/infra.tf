3753b92565a939c286931080e8ed35d13ea5c9df
Mociofleaca Alin <mociofleacaalin@Mociofleacas-MacBook-Pro.local>
2024-03-05 08:49:32 +0200

    add zabbix bootstrap and hosts

---
+++ secure_zone/infra.tf
@@ -0,0 +1,32 @@
+terraform {
+    backend "s3" {
+      bucket = "zabbix-demo-project-terraform-state"
+      key    = "terraform.tfstate"
+      region = "eu-west-2"
+    }
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0.0"
+      }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
+locals {
+  production_availability_zones = ["${var.region}a", "${var.region}b", "${var.region}c"]
+}
+
+module "networking" {
+  source = "./modules/networking"
+
+  region               = "${var.region}"
+  environment          = "${var.environment}"
+  vpc_cidr             = "${var.vpc_cidr}"
+  public_subnets_cidr  = "${var.public_subnets_cidr}"
+  private_subnets_cidr = "${var.private_subnets_cidr}"
+  availability_zones   = "${local.production_availability_zones}"
+}

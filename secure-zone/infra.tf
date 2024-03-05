d3afb86af5c1db99d3f092fe30a4f2a165321389
Mociofleaca Alin <mociofleacaalin@Mociofleacas-MacBook-Pro.local>
2024-03-05 08:47:02 +0200

    terraform apply action

--- secure-zone/infra.tf
+++ secure-zone/infra.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "s3" {
+      bucket = "zabbix-demo-project-terraform-state"
+      key    = "terraform.tfstate"
+      region = "eu-west-2"
+    }
+
     required_providers {
       aws = {
         source = "hashicorp/aws"
